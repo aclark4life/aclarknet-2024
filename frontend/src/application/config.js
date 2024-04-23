4e4e5c1fc5c6867a83f1f6ec06aa79bd24ec5d18
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-04-23 13:39:06 -0400

    Update frontend app

--- frontend/src/application/config.js
+++ frontend/src/application/config.js
@@ -1,2 +1,7 @@
-import '../utils/themeToggler.js';
-import '../utils/tinymce.js';
+import "../utils/chart.js";
+import "../utils/hijack.js"
+import "../utils/selected.js"
+import "../utils/sorttable.js"
+import "../utils/spark.js"
+import "../utils/themeToggler.js"
+import "../utils/image.js"
--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -6,23 +6,31 @@ function handleLogout() {
     window.location.href = '/accounts/logout';
 }
 
-const UserMenu = ({ isAuthenticated, isSuperuser, textColor }) => {
+const UserMenu = ({ isAuthenticated, isSuperuser }) => {
   return (
     <div> 
       {isAuthenticated ? (
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <i className="fa-solid fa-circle-user"></i>
+        <li className="nav-item dropdown text-nowrap">
+          <a className="nav-link text-light text-uppercase exclude-from-hover" type="button" aria-expanded="false" href="/dashboard">
+            <i className="fa-solid fa-leaf me-1"></i> 
+            <span className="d-none d-lg-inline">Dashboard</span>
+            <span className="visually-hidden">Link text</span>
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="/user/profile">Profile</a></li>
-            <li><a className="dropdown-item" href="/modelformtest/test-models">Model Form Test</a></li>
             {isSuperuser ? (
               <>
                 <li><hr className="dropdown-divider"></hr></li>
-                <li><a className="dropdown-item" href="/django" target="_blank">Django admin</a></li>
                 <li><a className="dropdown-item" href="/wagtail" target="_blank">Wagtail admin</a></li>
+                <li><a className="dropdown-item" href="/django" target="_blank">Django admin</a></li>
                 <li><a className="dropdown-item" href="/api" target="_blank">Django REST framework</a></li>
+                <li><a className="dropdown-item" href="https://analytics.google.com" rel="noreferrer" target="_blank">Google Analytics</a></li>
+                <li><a className="dropdown-item" href="https://admin.google.com/ac/home?hl=en" rel="noreferrer" target="_blank">Google Workspace</a></li>
+                <li><a className="dropdown-item" href="https://aclarknet.signin.aws.amazon.com/console" rel="noreferrer" target="_blank">AWS Console</a></li>
+                <li><a className="dropdown-item" href="/lounge" target="_blank">Lounge</a></li>
+                <li><a className="dropdown-item" href="/dashboard/user">Users</a></li>
+                <li><a className="dropdown-item" href="/blog">Blog</a></li>
+                <li><a className="dropdown-item" href="/modelformtest/test-models">Marketing</a></li>
               </>
             ) : null}
             <li><hr className="dropdown-divider"></hr></li>
@@ -31,7 +39,10 @@ const UserMenu = ({ isAuthenticated, isSuperuser, textColor }) => {
         </li>
       ) : (
         <li className="nav-item">
-          <a className={`nav-link text-${textColor}`} href="/accounts/login"><i className="fa-solid fa-circle-user"></i></a>
+          <a className="nav-link text-light text-uppercase exclude-from-hover" href="/accounts/login">
+            <i className="fa-solid fa-leaf me-1"></i> 
+            <span className="visually-hidden">Link text</span>
+          </a>
         </li>
       )}
     </div>
@@ -41,7 +52,6 @@ const UserMenu = ({ isAuthenticated, isSuperuser, textColor }) => {
 UserMenu.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
   isSuperuser: PropTypes.bool.isRequired,
-  textColor: PropTypes.string,
 };
 
 export default UserMenu;
