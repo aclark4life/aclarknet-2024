84450d85bea442d0b42d2914b601637d22758d79
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-04-24 12:58:10 -0400

    Custom db-init

--- project.mk
+++ project.mk
@@ -1,1 +1,8 @@
 PROJECT_NAME := aclarknet
+
+db-init-hstore:
+	psql $(PROJECT_NAME) -c 'CREATE EXTENSION IF NOT EXISTS hstore;'
+	psql test_$(PROJECT_NAME) -c 'CREATE EXTENSION IF NOT EXISTS hstore;'
+
+db-init: db-pg-init-default db-pg-init-test-default db-init-hstore
+
