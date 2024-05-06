9d517388aac5cb580885b23343811a5aa48aff0a
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-06 12:07:08 -0400

    Add JS to change drop down menu selections

---
+++ frontend/src/utils/menu.js
@@ -0,0 +1,11 @@
+const invoice = document.getElementById('id_invoice')
+const task = document.getElementById('id_task')
+const project = document.getElementById('id_project')
+const client = document.getElementById('id_client')
+
+invoice.addEventListener('change', function() {
+    var selectedIndex = this.selectedIndex;
+    task.selectedIndex = selectedIndex;
+    project.selectedIndex = selectedIndex;
+    client.selectedIndex = selectedIndex;
+});
