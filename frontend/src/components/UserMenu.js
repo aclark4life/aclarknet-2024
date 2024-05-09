96eab8f011a1b3bd2695e6c166b8cb74a94ae1cb
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-09 15:25:32 -0400

    Move django admin to the top of the user menu

--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -22,7 +22,6 @@ const UserMenu = ({ isAuthenticated, isSuperuser }) => {
             {isSuperuser ? (
               <>
                 <li><hr className="dropdown-divider"></hr></li>
-                <li><a className="dropdown-item" href="/wagtail" target="_blank">Wagtail admin</a></li>
                 <li><a className="dropdown-item" href="/django" target="_blank">Django admin</a></li>
                 <li><a className="dropdown-item" href="/api" target="_blank">Django REST framework</a></li>
                 <li><a className="dropdown-item" href="https://analytics.google.com" rel="noreferrer" target="_blank">Google Analytics</a></li>
@@ -32,6 +31,7 @@ const UserMenu = ({ isAuthenticated, isSuperuser }) => {
                 <li><a className="dropdown-item" href="/dashboard/user">Users</a></li>
                 <li><a className="dropdown-item" href="/blog">Blog</a></li>
                 <li><a className="dropdown-item" href="/explorer" target="_blank">SQL Explorer</a></li>
+                <li><a className="dropdown-item" href="/wagtail" target="_blank">Wagtail admin</a></li>
               </>
             ) : null}
             <li><hr className="dropdown-divider"></hr></li>
