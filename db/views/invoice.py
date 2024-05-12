31a03504b043eab90af56e13be022fed3f40b394
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-11 20:32:56 -0400

    Lint

--- db/views/invoice.py
+++ db/views/invoice.py
@@ -51,7 +51,18 @@ class BaseInvoiceView(BaseView, UserPassesTestMixin):
     url_index = f"{model_name.lower()}_index"
     url_view = f"{model_name.lower()}_view"
     template_name = "edit.html"
-    exclude = ["contacts", "company", "start_date", "end_date", "due_date", "user", "client", "project", "task", "issue_date"]
+    exclude = [
+        "contacts",
+        "company",
+        "start_date",
+        "end_date",
+        "due_date",
+        "user",
+        "client",
+        "project",
+        "task",
+        "issue_date",
+    ]
 
     def test_func(self):
         return self.request.user.is_superuser
