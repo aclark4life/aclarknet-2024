ec1513788759149094ae3c7e9cceed123550403c
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-19 14:47:34 -0400

    Reorder

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,3 @@
-# admin.py
 from django.contrib.admin import AdminSite
 
 
@@ -10,17 +9,14 @@ class CustomAdminSite(AdminSite):
     def get_app_list(self, request, app_label=None):
         app_dict = self._build_app_dict(request)
         app_list = sorted(app_dict.values(), key=lambda x: x["name"].lower())
-
-        # Define the custom order of models for each app
         custom_model_order = {
             "newsletter": [
                 "Newsletter",
-                "Message",
                 "Subscription",
                 "Submission",
-            ],  # Replace with your actual app label and model names
+                "Message",
+            ],
         }
-
         for app in app_list:
             if app["app_label"] in custom_model_order:
                 app["models"].sort(
@@ -28,8 +24,6 @@ class CustomAdminSite(AdminSite):
                         x["object_name"]
                     )
                 )
-
         return app_list
-
 
 custom_admin_site = CustomAdminSite(name="custom_admin")
