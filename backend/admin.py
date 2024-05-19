d26a4dc1a5b9e1caa28fb95c4b6338174942d265
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-05-19 15:19:42 -0400

    Custom admin site breaks nav

--- backend/admin.py
+++ backend/admin.py
@@ -6,24 +6,40 @@ class CustomAdminSite(AdminSite):
     site_title = "Project Makefile"
     index_title = "Project Makefile"
 
+    # def get_app_list(self, request, app_label=None):
+    #     app_dict = self._build_app_dict(request)
+    #     app_list = sorted(app_dict.values(), key=lambda x: x["name"].lower())
+    #     custom_model_order = {
+    #         "newsletter": [
+    #             "Newsletter",
+    #             "Subscription",
+    #             "Submission",
+    #             "Message",
+    #         ],
+    #     }
+    #     for app in app_list:
+    #         if app["app_label"] in custom_model_order:
+    #             app["models"].sort(
+    #                 key=lambda x: custom_model_order[app["app_label"]].index(
+    #                     x["object_name"]
+    #                 )
+    #             )
+    #     return app_list
+
     def get_app_list(self, request, app_label=None):
-        app_dict = self._build_app_dict(request)
+        """
+        Return a sorted list of all the installed apps that have been
+        registered in this site.
+        """ 
+        app_dict = self._build_app_dict(request, app_label)
+        
+        # Sort the apps alphabetically. 
         app_list = sorted(app_dict.values(), key=lambda x: x["name"].lower())
-        custom_model_order = {
-            "newsletter": [
-                "Newsletter",
-                "Subscription",
-                "Submission",
-                "Message",
-            ],
-        }
+        
+        # Sort the models alphabetically within each app.
         for app in app_list:
-            if app["app_label"] in custom_model_order:
-                app["models"].sort(
-                    key=lambda x: custom_model_order[app["app_label"]].index(
-                        x["object_name"]
-                    )
-                )
+            app["models"].sort(key=lambda x: x["name"])
+        
         return app_list
 
 custom_admin_site = CustomAdminSite(name="custom_admin")
