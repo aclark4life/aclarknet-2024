a5548ce291ed38923f208f9658081715743b0d91
Jeffrey 'Alex' Clark <aclark@aclark.net>
2024-08-28 11:33:31 -0400

    Add/update aclarknet files

--- project.mk
+++ project.mk
@@ -7,7 +7,7 @@ db-init-hstore:
 	psql $(PROJECT_NAME) -c 'CREATE EXTENSION IF NOT EXISTS hstore;'
 	psql test_$(PROJECT_NAME) -c 'CREATE EXTENSION IF NOT EXISTS hstore;'
 
-db-init: db-pg-init-default db-pg-init-test-default db-init-hstore
+db-init: db-init-default db-init-hstore
 
 install:
 	$(MAKE) pip-install
