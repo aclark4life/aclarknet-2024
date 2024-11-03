4084236e3f625762b33a1b206acb268f99a0cd21
Jeffrey A. Clark <aclark@aclark.net>
2024-11-03 14:20:30 -0500

    Fix static

--- .platform/hooks/predeploy/00_static.sh
+++ .platform/hooks/predeploy/00_static.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /var/app/current/static
-source /var/app/venv/staging-LQM1lest/bin/activate && make static
+source /var/app/venv/staging-LQM1lest/bin/activate && python manage.py static
 chown -R webapp:webapp /var/app/current/static
